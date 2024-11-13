8f0936914a836aca65ea99fffb18299b9790048b
Kidus <kidus.z3rd@gmail.com>
2024-11-13 11:51:41 +0300

    update: home added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = 9000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+
+app.get('/', (req,res)=>{
+    res.send("Welcome to Node Route Test")
+})
 // POST route to receive and log data
 app.post('/notifyme', (req, res) => {
     console.log('Received POST request:', req.body); // Log the request body
